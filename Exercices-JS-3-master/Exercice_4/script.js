49300397676537bfc9052e13124614f7e46e955f
naimdeb <naimdebard02@gmail.com>
2024-11-18 14:37:54 +0100

    DONE : Ex 4 JS3

--- Exercices-JS-3-master/Exercice_4/script.js
+++ Exercices-JS-3-master/Exercice_4/script.js
@@ -1,2 +1,19 @@
 // Exercice 4
 // Dans le fichier HTML fourni, mettre les bordures des inputs en rouge si les mots de passe sont différents. Sinon les mettre en vert.
+
+const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirmPassword")
+const btnConfirm = document.querySelector("button")
+
+btnConfirm.addEventListener("click", handleClickConfirm)
+
+function handleClickConfirm() {
+
+    if(password.value = confirmPassword.value) {
+        password.style.border = "2px solid green"
+        confirmPassword.style.border = "2px solid green"
+    } else {
+        password.style.border = "2px solid red"
+        confirmPassword.style.border = "2px solid red"
+    }
+}
