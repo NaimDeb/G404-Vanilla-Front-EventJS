9bb5a042102e6414401702d25605285260bc38df
naimdeb <naimdebard02@gmail.com>
2024-11-18 14:25:18 +0100

    DONE : Ex 2 JS3

--- Exercices-JS-3-master/Exercice_2/script.js
+++ Exercices-JS-3-master/Exercice_2/script.js
@@ -1,2 +1,21 @@
 // Exercice 2
 // Dans le fichier HTML fourni, faire afficher ou masquer le texte en fonction de l'ancre.
+
+const texte = document.querySelector("#text")
+
+const btnAfficher = document.querySelector("a:first-of-type")
+const btnMasquer = document.querySelector("a:last-of-type")
+
+
+btnAfficher.addEventListener("click", handleClickAfficherTexte)
+btnMasquer.addEventListener("click", handleClickHideTexte)
+
+
+function handleClickAfficherTexte() {
+    texte.style.visibility = "visible"
+    
+}
+function handleClickHideTexte() {
+    texte.style.visibility = "hidden"
+
+}
