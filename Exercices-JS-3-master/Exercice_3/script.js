e52f9571bdd18f5a82382b8495fdc87fc4202bf1
naimdeb <naimdebard02@gmail.com>
2024-11-18 14:30:20 +0100

    DONE : Ex 3 JS3

--- Exercices-JS-3-master/Exercice_3/script.js
+++ Exercices-JS-3-master/Exercice_3/script.js
@@ -1,2 +1,15 @@
 // Exercice 3
 // Dans le fichier HTML fourni, changer la couleur du texte en fonction du bouton choisi.
+
+const texte = document.querySelector("#text")
+
+const colorButtons = document.querySelectorAll(".color")
+
+colorButtons.forEach((color) => {
+    color.addEventListener("click", handleClickChangeColor)
+})
+
+function handleClickChangeColor(event) {
+    texte.style.color = event.target.classList[1];
+    
+}
