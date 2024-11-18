61a070766efce5a903a256be76c73e7692764983
naimdeb <naimdebard02@gmail.com>
2024-11-18 11:57:45 +0100

    DONE : Ex 2 JS2

--- Exercices-JS-2-master/Exercice_2/script.js
+++ Exercices-JS-2-master/Exercice_2/script.js
@@ -1,5 +1,13 @@
 // Exercice 2
-// Dans le fichier HTML fourni, faire afficher ou masquer le texte en fonction de l'ancre.
+// Dans le fichier HTML fourni, afficher une boite de dialogue avec le message : "Merci de votre participation" quand le champ de saisie perd le focus.
 
 const textBox = document.querySelector("#lastname")
 
+console.log(textBox)
+
+// Ou blur
+textBox.addEventListener("focusout", handleOutOfFocusTextBox);
+
+function handleOutOfFocusTextBox() {
+    alert("Hallo")
+}
