311484d8b845f39cc8cfeac1ca10144ebcae661a
naimdeb <naimdebard02@gmail.com>
2024-11-19 10:11:33 +0100

    DONE : ex 3 Mainpage

---
+++ Exercices-JS-MainPage/3.CombinaisonGagnante/script.js
@@ -0,0 +1,22 @@
+// 🏆️ Combinaison gagnante :
+
+//     Créez un paragraphe avec une consigne de combinaison de 2 touches à appuyer.
+//     Lorsque la combinaison est pressée, une phrase apparaît dans un autre paragraphe.
+//     Bonus : si j'appuie sur une troisième touche, le texte change de couleur.
+
+
+
+const texteGagnant = document.querySelector("#texteGagnant")
+const divTexte = document.querySelector(".divTexte")
+
+
+window.addEventListener("keydown", handleKeysForText);
+
+function handleKeysForText(event) {
+    if(event.ctrlKey && event.key == "v"){
+        texteGagnant.style.visibility = "visible"
+    };
+    if(event.ctrlKey && event.shiftKey && event.key == "V"){
+        divTexte.style.color = "red"
+    };    
+}
